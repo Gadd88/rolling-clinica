f5b746b886c11ab27b1eb12dcf1e4f50048403b9
JoacooReyes <joacooreyes@gmail.com>
2024-01-04 13:08:36 -0300

    estoy arreglando los botones

--- src/js/scriptNuevo.js
+++ src/js/scriptNuevo.js
@@ -9,6 +9,10 @@ const cargarTabla = () => {
       <th>${item.ID}</th>
       <td>${item.nombrePaciente}</td>
       <td>${item.fechaHora}</td>
+      <td> <div class="d-flex gap-2">
+      <button class="btn btn-outline-warning"><i class="fa-solid fa-pen-to-square"></i></button>
+      <button class="btn btn-outline-danger"><i class="fa-solid fa-trash"></i></button>
+      </div></td>
     `;
     fila.innerHTML = celdas;
     cuerpoTabla.appendChild(fila); 

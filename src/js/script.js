1d6fa83b78acfe9415265ee160afe906ed8be904
GastonGLL <gastongutierrezllomplat@gmail.com>
2024-01-12 23:39:11 -0300

    Block Borrar

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+import listaTurnos from './turnos.json' assert { type : 'json'}
+
 const formTurnos = document.getElementById('formTurnos')
 
 const fecha = document.getElementById('fecha')
@@ -11,21 +13,21 @@ const turnosPaciente = []
 formTurnos.addEventListener('submit', (e) => {
   e.preventDefault()
   const turno = {
-    "id": "",
+    "id": crypto.randomUUID().slice(0, 4),
     "hora": hora.value,
     "fecha": fecha.value,
     "motivo": motivo.value,
     "medico": medico.value
   }
-  turnosPaciente.push(turno)
+  listaTurnos.push(turno)
   formTurnos.reset()
   cargarTurnos()
-  console.log(turnosPaciente);
+  console.log(listaTurnos);
 })
 
 const cargarTurnos = () => {
-  cuerpoTurnos.innerHTML=''
-  turnosPaciente.forEach(turno => {
+  cuerpoTurnos.innerHTML = ''
+  listaTurnos.forEach(turno => {
     const fila = document.createElement('tr')
     const celdas = `<th>${turno.fecha}</th>
     <td>${turno.hora}</td>
@@ -33,7 +35,7 @@ const cargarTurnos = () => {
     <td>${turno.motivo}</td>
     <td>
     <div class="d-flex gap-2">
-    <button class="btn btn-outline-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+    <button onclick="borrarTurno(${turno.id})" class="btn btn-outline-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
     </div>
     </td>
     `
@@ -41,4 +43,9 @@ const cargarTurnos = () => {
     fila.innerHTML = celdas
     cuerpoTurnos.appendChild(fila)
   })
+}
+
+window.borrarTurno = (id) => {
+  let index = listaTurnos.findIndex((turno) => turno.id == id)
+  console.log(index);
 }

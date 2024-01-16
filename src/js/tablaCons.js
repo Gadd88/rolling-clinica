03a8c57d791239cd595a69288416dffef9df1eb3
Lorena <mlore20@hotmail.com>
2024-01-15 23:24:46 -0300

    descripciones

--- src/js/tablaCons.js
+++ src/js/tablaCons.js
@@ -2,19 +2,23 @@
 //TRAE LOS DATOS DEL LOCALSTORAGE//
 
 const mostrarDatosLocalStorage = () => {
-  const tablaCuerpo = document.getElementById('tablaConsultasBody');
-  tablaCuerpo.innerHTML = ''; // Limpiar el contenido actual de la tabla
+const tablaCuerpo = document.getElementById('tablaConsultasBody');
+
+  tablaCuerpo.innerHTML = ''; // Limpiar el contenido actual de la tabla //
 
   const consultasGuardadas = JSON.parse(localStorage.getItem('consultas')) || [];
 
+  //Se agrega una fila a la tabla por cada consulta que ingresa //
   consultasGuardadas.forEach((consulta) => {
-    const fila = tablaCuerpo.insertRow();
+  const fila = tablaCuerpo.insertRow();
 
-    Object.values(consulta).forEach((valor, index) => {
+  Object.values(consulta).forEach((valor, index) => {
       const celda = fila.insertCell();
 
-      if (index === 3) { // Si es la columna de consultas
-        // Botón para mostrar la consulta en SweetAlert
+      if (index === 3) { 
+
+        // Botón para mostrar la consulta en SweetAlert//
+
         const btnMostrarConsulta = document.createElement('button');
         btnMostrarConsulta.className = 'btn btn-primary btn-sm';
         const iconoMostrarConsulta = document.createElement('img');
@@ -23,20 +27,18 @@ const mostrarDatosLocalStorage = () => {
         btnMostrarConsulta.addEventListener('click', () => {
           mostrarConsultaSweetAlert(consulta);
         });
-
-        // Agregar el ícono al botón
         btnMostrarConsulta.appendChild(iconoMostrarConsulta);
 
         // Insertar el botón en la celda
         celda.appendChild(btnMostrarConsulta);
-      } else if (index !== 4) { // Evitar la columna "acciones"
+      } else if (index !== 4) { 
         celda.textContent = valor;
       }
     });
   });
 };
 
-
+// Muestra una alerta de SweetAlert con el contenido de la consulta y opciones para responder o eliminar//
 const mostrarConsultaSweetAlert = (consulta) => {
   Swal.fire({
     title: "Consulta del usuario",
@@ -55,7 +57,7 @@ const mostrarConsultaSweetAlert = (consulta) => {
     }
   });
 };
-
+ //Responder consulta //
 const responderConsultaSweetAlert = (consulta) => {
   Swal.fire({
     title: "Responder la consulta",
@@ -92,6 +94,7 @@ const responderConsultaSweetAlert = (consulta) => {
   });
 };
 
+//Eliminar consulta //
 const eliminarConsultaSweetAlert = (consulta) => {
   Swal.fire({
     title: '¿Estás seguro de eliminar la consulta?',
@@ -116,15 +119,15 @@ const eliminarConsultaSweetAlert = (consulta) => {
   });
 };
 
+//Obtiene las consultas almacenadas, se encuentra la consulta a eliminar y la elimina del array //
+Actualiza el localStorage con la nueva lista de consultas
 const borrarConsulta = (consulta) => {
   const consultasGuardadas = JSON.parse(localStorage.getItem('consultas')) || [];
   const indice = consultasGuardadas.findIndex((c) => c.nombre === consulta.nombre && c.telefono === consulta.telefono && c.mail === consulta.mail && c.consulta === consulta.consulta);
-
+//Se actualiza el localStorage //
   if (indice !== -1) {
     consultasGuardadas.splice(indice, 1);
     localStorage.setItem('consultas', JSON.stringify(consultasGuardadas));
   }
 };
-
-// Llamada inicial para mostrar los datos almacenados al cargar la página
 mostrarDatosLocalStorage();

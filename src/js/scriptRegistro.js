176edcd4e14ae73c1d6cd6a10b7a78e9e58a17b5
Pablo Moyano <pablomoyano08@gmail.com>
2024-01-19 18:56:35 -0300

    actualizacion de pagina regitro

--- src/js/scriptRegistro.js
+++ src/js/scriptRegistro.js
@@ -1,27 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const registrationForm = document.getElementById('registration-form');
-  
-    registrationForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      // Obtén el tipo de usuario seleccionado
-      const userType = document.querySelector('input[name="userType"]:checked').value;
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-  
-      // Crea un objeto de usuario
-      const userData = {
-        userType,
-        name,
-        email,
-        password
-      };
-  
-      // Guarda los datos en el almacenamiento local
-      localStorage.setItem(userType, JSON.stringify(userData));
-  
-      // Redirige al usuario a su página correspondiente
-      window.location.href = userType === 'doctor' ? 'doctor.html' : 'patient.html';
-    });
+  const formularioRegistro = document.getElementById('registration-form');
+
+  formularioRegistro.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+
+    // Validacion de contraseñas
+    const contraseña = document.getElementById('password').value;
+    const confirmarContraseña = document.getElementById('confirm-password').value;
+    if (contraseña !== confirmarContraseña) {
+      alert('Las contraseñas no coinciden.');
+      return;
+    }
+
+    // Los datos del formulario
+    const nombreCompleto = document.getElementById('full-name').value;
+    const correoElectronico = document.getElementById('email').value;
+    const dni = document.getElementById('dni').value;
+    const tipoUsuario = document.querySelector('input[name="userType"]:checked').value;
+    const domicilio = document.getElementById('address').value;
+    const codigoPostal = document.getElementById('postal-code').value;
+    const telefono = document.getElementById('phone').value;
+
+    // Crea un objeto de usuario
+    const datosUsuario = {
+      nombreCompleto,
+      correoElectronico,
+      dni,
+      tipoUsuario,
+      contraseña,
+      domicilio,
+      codigoPostal,
+      telefono
+    };
+
+    // Guarda los datos en el almacenamiento local
+    localStorage.setItem(tipoUsuario + dni, JSON.stringify(datosUsuario));
+
+    // Redireccion de la pagina
+    window.location.href = tipoUsuario === 'medico' ? 'medico.html' : 'paciente.html';
   });
+
+  // boton reset
+  botonReset.addEventListener('click', function(evento) {
+    const confirmacion = confirm('¿Estás seguro de que deseas limpiar el formulario?');
+    if (!confirmacion) {
+      evento.preventDefault();
+    }
+  });
+});

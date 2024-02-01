867f7696c5ef576dc05c50b6bafaa6b106191a3e
JoacooReyes <joacooreyes@gmail.com>
2024-02-01 02:12:09 -0300

    agrego los cambios esteticos e intento unir mi CRUD con el de turnos pacientes

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,138 @@
-import listaPacientes from '../prueba/turnosPrueba.json' assert{type:"json"};
-const turnosStorage = JSON.parse(localStorage.getItem('listaPacientes'));
+const formTurnos = document.getElementById('formTurnos')
 
-const mostrarTurnos = () => {
-  const tablaTurnos = document.getElementById('tablaTurnos');
-  tablaTurnos.innerHTML = '';
+const fecha = document.getElementById('fecha')
+const hora = document.getElementById('hora')
+const medico = document.getElementById('medico')
+const motivo = document.getElementById('motivo')
+const tablaTurnos = document.getElementById('tablaTurnos')
 
-  if (turnosStorage.length < 1) {
-    return tablaTurnos.innerHTML = '<h3>No hay turnos agendados</h3>';
-  } else{
+let listaTurnos = []
 
-  let cuerpo = '';
-  turnosStorage.forEach((turno, index) => {
-    const { nombrePaciente, fechaHora, ID } = turno;
-    cuerpo += '<tr>';
-    cuerpo += '<td>' + ID + '</td>';
-    cuerpo += '<td>' + nombrePaciente + '</td>';
-    cuerpo += '<td>' + fechaHora + '</td>';
-    cuerpo +=
-      '<td><button onclick="deleteData(' +
-      index +
-      ')" class="btn btn-danger">Eliminar</button></td>';
-    cuerpo += '</tr>';
-  });
-  document.querySelector('#tablaTurnos').innerHTML = cuerpo;
-//   tablaTurnos.innerHTML = cuerpo;
+// Evento que crea un objeto llamado turno, y lo carga en un JSON mediante el formulario.
+
+formTurnos.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const turno = {
+    "id": crypto.randomUUID().slice(0, 4),
+    "hora": hora.value,
+    "fecha": fecha.value,
+    "motivo": motivo.value,
+    "medico": medico.value
+  }
+  listaTurnos.push(turno)
+  formTurnos.reset()
+  localStorage.setItem('turnos', JSON.stringify(listaTurnos))
+  cargarTurnos()
+  console.log(listaTurnos);
+})
+
+// Función que carga los turnos que se van creando mediante el formulario.
+
+const cargarTurnos = () => {
+  tablaTurnos.innerHTML = ''
+  listaTurnos.forEach(turno => {
+    const fila = document.createElement('tr')
+    const celdas = `<th>${turno.id}</th>
+    <td>${turno.hora}</td>
+    <td>${turno.fecha.hora}</td>
+    <td>${turno.motivo}</td>
+    <td> <div class="d-flex gap-2">
+      <button class="btn btn-outline-warning"><i class="fa-solid fa-pen-to-square"></i></button>
+      <button class="btn btn-outline-danger borrar"><i class="fa-solid fa-trash"></i></button>
+      </div></td>
+    `
+
+    fila.innerHTML = celdas
+    tablaTurnos.appendChild(fila)
+  })
+}
+
+// Función que elimina los turnos del DOM y del local storage.
+
+window.borrarTurno = (id) => {
+  let index = listaTurnos.findIndex((turno) => turno.id == id)
+  let validar = confirm(`¿Está seguro/a que desea cancelar su turno del día?`)
+
+  if (validar) {
+    listaTurnos.splice(index, 1)
+    localStorage.setItem('turnos', JSON.stringify(listaTurnos))
+    cargarTurnos()
+  }
+}
+
+// Función que carga los turnos del local storage.
+
+const cargaDeTurnos = () => {
+  const baseDeDatos = JSON.parse(localStorage.getItem('turnos'))
+
+  if (!baseDeDatos) {
+    localStorage.setItem('turnos', JSON.stringify(listaTurnos))
+  } else {
+    cuerpoTurnos.innerHTML = ''
+    JSON.parse(localStorage.getItem('turnos')).forEach(turno => {
+      const fila = document.createElement('tr')
+      const celdas = `<th>${turno.id}</th>
+    <td>${turno.fecha}</td>
+    <td>${turno.hora}</td>
+    <td>${turno.motivo}</td>
+    <td>
+      <div class="d-flex gap-2">
+        <button onclick="borrarTurno('${turno.id}')" class="btn btn-outline-danger">
+          <i class="fa fa-trash-o" aria-hidden="true"></i>
+        </button>
+      </div>
+    </td>
+    `
+      fila.innerHTML = celdas
+      tablaTurnos.appendChild(fila)
+    })
+  }
+}
+
+// Función que se ejecuta al inicio.
+
+cargaDeTurnos()
+
+// Boton para borrar turno
+
+const borrarTurno = (event) => {
+  const filaTurno = event.target.closest('tr'); 
+  Swal.fire({
+      title: 'Estas seguro que quieres eliminar este turno?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      denyButtonText: 'No',
+      customClass: {
+        actions: 'my-actions',
+        cancelButton: 'order-1 right-gap',
+        confirmButton: 'order-2',
+        denyButton: 'order-3',
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+          filaTurno.remove();
+        Swal.fire('Eliminado', '', 'success')
+
+        listaTurnos.addEventListener('click', (e) => {
+          if(e.target.classList.contains('borrar') || e.target.parentElement.classList.contains('borrar')){
+              const turnoId = e.target.closest('tr').ID
+              let newItem = listaTurnos.filter(item => item.ID != turnoId) 
+              listaTurnos = newItem; 
+              cargaDeTurnos();
+              
+          }
+      })
+      
+
+
+      } else if (result.isDenied) {
+        Swal.fire('No eliminaste este turno!', '', 'info')
+      }
+    })
+
 };
-document.onload = mostrarTurnos();
 
-
- document.addEventListener('DOMContentLoaded', () => {
-   mostrarTurnos();
- 
- });}
-
-// document.querySelector('#tablaTurnos').innerHTML = cuerpo;
-
-let validar=confirm('Estas seguro que deseas eliminar este turno?')
-    if (validar){
-    filaTurno.remove(); 
-    }
+document.querySelectorAll('.borrar').forEach(botonBorrar => {
+  botonBorrar.addEventListener('click', borrarTurno);
+});
